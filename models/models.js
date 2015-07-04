cf27e6f834caf8adc958a7c5a711da3258b96cd6
josecarlos.castillo <jc.castillo.hernande@bbva.com>
2015-07-04 16:17:27 +0200

    Nueva pregunta

--- models/models.js
+++ models/models.js
@@ -40,6 +40,10 @@ sequelize.sync().then(function(){
         respuesta: 'Roma'
       });
       Quiz.create({
+        pregunta: 'Capital de España',
+        respuesta: 'Madrid'
+      });
+      Quiz.create({
         pregunta: 'Capital de Portugal',
         respuesta: 'Lisboa'
       }).then(function(){console.log('Base de datos inicializada')});
